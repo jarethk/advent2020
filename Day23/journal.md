66e600753670342dab7fa3e4bb3012c9c7a5b088
David Fuller <jarethk@shadowwebs.org>
2023-11-11 10:53:53 -0500

    Day 23 part 1, easy enough

---
+++ Day23/journal.md
@@ -0,0 +1,3 @@
+Part 1 - seems like a fairly straight-forward cycle.  They talk like a circle queue, but the current cup is always going to be "next in line" so we can just keep shifting off the front.  Take off the first as the current, slice off the next 3 as the 3 moving, and figure out where those 3 get inserted, then add the current to the end of the queue.
+
+Part 2 - 
---
+++ Day23/part1.mjs
@@ -0,0 +1,38 @@
+/* eslint-disable no-unused-vars */
+import { open } from 'node:fs/promises';
+
+const debug = 0;
+const dl = log => { if (debug) console.log(log); };
+
+//const input = "389125467";
+const input = "853192647";
+const MAX_CYCLE = 100;
+
+const t0 = performance.now();
+
+let cups = input.split("").map(c => ~~c);
+
+for (let cycle = 0; cycle < MAX_CYCLE; cycle++) {
+    dl(`cycle ${cycle} start: ${JSON.stringify(cups)}`)
+    let current = cups.shift();
+    let removed = cups.splice(0, 3);
+    dl(`cycle ${cycle} removed: ${JSON.stringify(cups)}`)
+    let scups = [...cups].sort((a, b) => b - a);
+    let destination = scups.find(v => v <= current - 1);
+    dl(`destination: ${destination} from ${current - 1} in ${JSON.stringify(scups)}`)
+    if (destination == undefined) destination = scups[0];
+    dl(`destination: ${destination}`)
+    let dindex = cups.indexOf(destination);
+    cups.splice(dindex + 1, 0, ...removed);
+    dl(`cycle ${cycle} readded: ${JSON.stringify(cups)}`)
+    cups.push(current);
+}
+
+console.log(`Cups after ${MAX_CYCLE} cycles: ${JSON.stringify(cups)}`)
+let removed = cups.splice(0, cups.indexOf(1));
+cups.shift();
+cups.push(...removed);
+console.log(`Final order: ${cups.join("")}`);
+
+const t1 = performance.now();
+console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
